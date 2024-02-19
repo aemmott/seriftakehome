416f079d01d96a45b421d38a922917f0ec78d8ae
aemmott <andrew.f.emmott@gmail.com>
2024-02-19 10:37:38 -0800

    Initial for loop; test prints out data on first PPO-having instance

--- findppo.py
+++ findppo.py
@@ -6,8 +6,24 @@ from collections import defaultdict as ddict
 import sys
 
 def isolate_ppo(state, uri):
-    print('Received state code: ' + state)
-    print('Received uri: ' + uri)
+    uri_stream = smart_open.open(uri)
+    data_stream = json_stream.load(uri_stream)
+    reporting_structure_stream = data_stream['reporting_structure']
+    decoder = json.JSONDecoder()
+    for ro_streamed in reporting_structure_stream:
+        ro_dump = json.dumps(ro_streamed, default=stream_enc_default)
+        ro = ddict(list, decoder.decode(ro_dump))
+        has_ppo = False
+        for p in ro['reporting_plans']:
+            plan_name = p['plan_name'].split('-', 1)[0]
+            name_tokens = plan_name.split(' ')
+            if state in name_tokens and 'PPO' in name_tokens:
+                has_ppo = True
+                break
+        if has_ppo:
+            print(ro['reporting_plans'])
+            print(ro['in_network_files'])
+            break
 
 
 if __name__ == "__main__":
